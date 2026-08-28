c7b326151ae11dc6c5a1ec3cf4e625424610e225
Luiza Desyree <luiza.desyree@aluno.impacta.edu.br>
2026-08-28 01:43:47 -0300

    Create exercício3.py

---
+++ exercício3.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um número: "))
+
+antecessor = numero - 1
+sucessor = numero + 1
+
+print("Antecessor:", antecessor)
+print("Número:", numero)
+print("Sucessor:", sucessor)
