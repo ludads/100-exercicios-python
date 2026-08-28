8ed2af7a113a67e4b4df19697039a4d98b56bfc0
Luiza Desyree <luiza.desyree@aluno.impacta.edu.br>
2026-08-28 01:49:24 -0300

    Create exercício9.py

---
+++ exercício9.py
@@ -0,0 +1,7 @@
+salario = float(input("Salário atual: R$ "))
+
+aumento = salario * 0.15
+novo_salario = salario + aumento
+
+print(f"Aumento: R$ {aumento:.2f}")
+print(f"Novo salário: R$ {novo_salario:.2f}")
