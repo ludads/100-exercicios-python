cc4e065b34c3737327bb67ef8487562620afcb63
Luiza Desyree <luiza.desyree@aluno.impacta.edu.br>
2026-08-28 01:47:04 -0300

    Add area and perimeter calculations for a rectangle

---
+++ exercício6.py
@@ -0,0 +1,8 @@
+largura = float(input("Largura: "))
+altura = float(input("Altura: "))
+
+area = largura * altura
+perimetro = 2 * (largura + altura)
+
+print("Área:", area)
+print("Perímetro:", perimetro)
